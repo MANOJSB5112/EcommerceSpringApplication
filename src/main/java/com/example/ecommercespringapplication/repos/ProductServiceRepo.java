42b4ab010bcec1d79bf1d641395296881e6a0ee9
MANOJ S B <manojsb5112@gmail.com>
2024-06-15 18:32:43 +0530

    created repos package and created ProductServiceRepo interface which extends JpaRepository<,>

---
+++ src/main/java/com/example/ecommercespringapplication/repos/ProductServiceRepo.java
@@ -0,0 +1,9 @@
+package com.example.ecommercespringapplication.repos;
+
+import com.example.ecommercespringapplication.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductServiceRepo extends JpaRepository<Product,Long> {
+
+
+}
